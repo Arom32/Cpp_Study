e7a2d373238989382d19e79a73cb17ff813a221e
Jihyun Lee <jhhyun2005@naver.com>
2026-04-01 21:12:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){    
+    int t;    
+    std::cin >> t;
+    
+    for(int i = 1; i <= t; i++ ){
+        std::cout << std::string(i, '*') << "\n";
+    }
+    
+    return 0;
+}
