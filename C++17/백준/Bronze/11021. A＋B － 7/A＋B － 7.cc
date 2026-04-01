e4658e681235c83a593245838bc2213c3d5738d8
Jihyun Lee <jhhyun2005@naver.com>
2026-04-01 21:00:54 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(void){
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    
+    int t,a,b;
+    
+    std::cin >> t;
+    
+    for (int i = 0 ; i < t ; i++ ){
+        std::cin >> a >> b;
+        std::cout<< "Case #" << i+1 << ": "<<a+b<<"\n";
+    }
+    
+    
+    return 0;
+}
